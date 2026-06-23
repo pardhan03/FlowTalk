62eba205663e8061ac5b7c2905d559e9785e89f6
Manish Pardhan <pardhanmanish7887@gmail.com>
2026-06-24 00:53:09 +0530

    added channel and thread screen

---
+++ src/channel/[cid]/thread/[cid]/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ThreadScreen = () => {
+  return (
+    <View>
+      <Text>ThreadScreen</Text>
+    </View>
+  )
+}
+
+export default ThreadScreen
+
+const styles = StyleSheet.create({})

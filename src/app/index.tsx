da0ad7bc69f8021e74e7efd9c05af7d1333308c7
Manish Pardhan <pardhanmanish7887@gmail.com>
2026-06-23 12:11:22 +0530

    tailwind issue fix

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
-import '../../global.css';
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="text-xl font-bold text-blue-500">
-      <Text>Edit src/app/index.tsx to edit this screenm.</Text>
+    <View className="flex-1 items-center justify-center bg-slate-100">
+      <Text className="text-2xl font-bold text-blue-600">
+        Edit src/app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
